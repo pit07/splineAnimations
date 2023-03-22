f58a541e6201cb68dd6e84bc0722a748be595a09
Vincent <vincent.blondaux@gmail.com>
2023-03-22 19:10:56 +0100

    Commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="canvas3d"></div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  mounted(){
+    var runtime = require("@splinetool/runtime")
+
+  }
+}
+</script>
